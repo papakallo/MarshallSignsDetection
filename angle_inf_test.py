e1285d3e9a4b4cde1684392cce7a8977871ddc05
Papakallo <paviveerar@gmail.com>
2026-06-01 22:18:45 +0200

    use atan2

--- angle_inf_test.py
+++ angle_inf_test.py
@@ -7,7 +7,8 @@ import math
 
 # joint pairs , angles
 JOINT_CONSTANTS = {
-        "1": [("12-14", "14-16"), (-90)]
+        "1": [("12-14", "14-16"), (-90)],
+        # "2": [("12-14", "14-16", "16-22"), (0, 0, -90)]
         }
 
 ANGLE_THRESHOLD = 15 # degrees
@@ -21,16 +22,19 @@ def make_decision(calc_angles: list):
 
 def check_requirements(req: list, calc_angles: list) -> bool:
     if isinstance(req[1], int):
-        angle = req[1]
+        angles = [req[1]]
+    else:
+        angles = req[1]
 
     conds : bool = []
     for pair in req[0]:
-        try:
-            conds.append(calc_angles[pair] < (angle + ANGLE_THRESHOLD) and calc_angles[pair] > (angle - ANGLE_THRESHOLD))
-            print(f"ANGLES {pair}: {calc_angles[pair]}\n")
-        except:
-            print("Angles not found")
-            return False
+        for angle in angles:
+            try:
+                conds.append(calc_angles[pair] < (angle + ANGLE_THRESHOLD) and calc_angles[pair] > (angle - ANGLE_THRESHOLD))
+                print(f"ANGLES {pair}: {calc_angles[pair]}\n")
+            except:
+                print("Angles not found")
+                return False
 
     if all(conds):
         return True
@@ -70,16 +74,16 @@ while cap.isOpened():
                 next_landmark = landmark_list[idx+2]
                 diff_y = next_landmark.y - landmark.y
                 diff_x = next_landmark.x - landmark.x
-                angle = math.degrees(math.atan(diff_y/diff_x))
+                angle = math.degrees(math.atan2(diff_y, diff_x))
                 current_angles[f"{idx}-{idx+2}"] = angle
 
                 # hand joints
-                if idx == 16 or 15:
+                if idx in [15, 16]:
 
                     next_landmark = landmark_list[idx+4]
                     diff_y = next_landmark.y - landmark.y
                     diff_x = next_landmark.x - landmark.x
-                    angle = math.degrees(math.atan(diff_y/diff_x))
+                    angle = math.degrees(math.atan2(diff_y, diff_x))
                     current_angles[f"{idx}-{idx+4}"] = angle
 
     else:
